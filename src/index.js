dd12d94903408f996cffcd7514aa715097e42a74
themaheshdubey <themahesh.mdubey@gmail.com>
2024-05-28 21:56:16 +0530

    Setup dotenv

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const {PORT} = require("./config/serverConfig");
+
+
+const setupAndStartServer = async () => {
+
+    const app = express();
+
+    app.listen(PORT , () => {
+        console.log(`server is running on port ${PORT}`);
+    });
+
+}
+
+setupAndStartServer();
